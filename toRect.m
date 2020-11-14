a73a1f547c66ec474512a19dd9b0ba8efabd4eed
michealcarac <52177325+michealcarac@users.noreply.github.com>
2020-11-13 21:22:00 -0500

    Update toRect.m

--- toRect.m
+++ toRect.m
@@ -1,6 +1,8 @@
+%Author: Micheal Caracciolo
+%@michealcarac on github
 function toRect(A,theta) %IN DEGREES, RAD --> DEGREES = rad*180/pi
 rad = theta*pi/180;
-a = A*cos(rad)
+a = A*cos(rad);
 b = A*sin(rad);
 if a < .00001 & a > 0 %Otherwise matlab will give you something like 1*10^-15 instead of zero, remove for higher precision
     a = 0;
